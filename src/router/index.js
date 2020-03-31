61c5f7883a73f11cef86d9d26cebc84c1f5ca606
wuyax <pengqingkai@gmail.com>
2020-03-31 15:21:23 +0800

    新增vuex

--- src/router/index.js
+++ src/router/index.js
@@ -27,6 +27,11 @@ const routes = [
     path: '/vuex',
     name: 'vuex',
     component: () => import('../views/startup-vuex/index.vue')
+  },
+  {
+    path: '/avuex',
+    name: 'avuex',
+    component: () => import('../views/startup-vuex/vuex-map.vue')
   }
 ]
 
---
+++ src/views/startup-vuex/vuex-map.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="startup-vuex">
+    <h2>Mutation</h2>
+    <p>count:{{count}}</p>
+    <button @click="decrease">-</button>
+    <button @click="increase">+</button>
+    <h2>Action</h2>
+    <button @click="decreaseAsync">-(async)</button>
+    <button @click="increaseAsync">+(async)</button>
+  </div>
+</template>
+<script>
+import {mapState, mapMutations, mapActions} from 'vuex'
+// https://cn.vuejs.org/v2/style-guide/#组件-实例的选项的顺序-推荐
+export default {
+  name: 'StartupVuex',
+  components: {},
+  model: {},
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {
+    ...mapState(['count'])
+  },
+  watch: {},
+  created() {},
+  mounted() {},
+  beforeDestroy() {},
+  methods: {
+    ...mapMutations(['decrement', 'increment']),
+    ...mapActions(['decrementAsync', 'incrementAsync']),
+    decrease() {
+      this.decrement()
+    },
+    increase() {
+      this.increment()
+    },
+    decreaseAsync() {
+      this.decrementAsync()
+    },
+    increaseAsync() {
+      this.incrementAsync()
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+</style>
